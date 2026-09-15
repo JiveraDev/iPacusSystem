beb3b3fec08b643745a2b647d83f6c55f538e703
JiverDev <lorenceolivera1039@gmail.com>
2026-09-15 13:08:27 +0800

    deploy: update Hostinger site (2026-09-15 05:08:27 UTC)

---
+++ assets/vetDiagnosisService-DRnEiFM7.js
@@ -0,0 +1,1 @@
+import{nt as e,ut as t}from"./index-BKMcCT0N.js";function n(t={}){let n=t instanceof URLSearchParams?t.toString():new URLSearchParams(t).toString();return e(`/vet-diagnoses${n?`?${n}`:``}`)}function r(e){return t(`/vet-diagnoses`,e)}export{n,r as t};

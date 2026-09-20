bdc3c7d0937c91c6d2aaddca1e335ea54cde07ad
JiverDev <lorenceolivera1039@gmail.com>
2026-09-20 17:21:39 +0800

    deploy: update Hostinger site (2026-09-20 09:21:39 UTC)

--- php/auth_google_complete.php
+++ php/auth_google_complete.php
@@ -128,6 +128,7 @@ try {
         $pdo->rollBack();
     }
 
+    googleAuthHandleSetupException($error);
     error_log('Google onboarding failed: ' . $error->getMessage());
     googleAuthJsonResponse(500, [
         'message' => 'Google registration could not be completed. Please try again.',

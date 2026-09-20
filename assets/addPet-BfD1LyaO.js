86b75030fff51d715482b510880913ec712089a6
JiverDev <lorenceolivera1039@gmail.com>
2026-09-20 19:01:46 +0800

    deploy: update Hostinger site (2026-09-20 11:01:46 UTC)

--- assets/addPet-BfD1LyaO.js
+++
@@ -1,1 +0,0 @@
-import{pt as e}from"./index-DC-5gYhb.js";async function t(t){try{return await e(`/pet_information`,t,{apiPrefix:!0})}catch(e){throw console.error(`Error in addPetService:`,e),e}}export{t};
